abd6fbef8b25bfce77befb0a3ea6941ad095e5e4
quocthoaitran <thoai.tran@orientsoftware.com>
2019-06-05 09:12:46 +0700

    Updates

---
+++ precache-manifest.36d01db325b0b0dd5d15acc588750210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6b360ea594c8e48a2b0f585e868ce0",
+    "url": "/quocthoaitran/dog-saga/index.html"
+  },
+  {
+    "revision": "0dd186b20452c9c55255",
+    "url": "/quocthoaitran/dog-saga/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "118ef9a323fd4945fa24",
+    "url": "/quocthoaitran/dog-saga/static/js/2.1980c0bf.chunk.js"
+  },
+  {
+    "revision": "0dd186b20452c9c55255",
+    "url": "/quocthoaitran/dog-saga/static/js/main.29180e6b.chunk.js"
+  },
+  {
+    "revision": "5056e2f6dff6e1a7223f",
+    "url": "/quocthoaitran/dog-saga/static/js/runtime~main.04ec1378.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/quocthoaitran/dog-saga/static/media/logo.5d5d9eef.svg"
+  }
+]);
